3f455d6b123f5969ae6f820f54a9042e96ccb541
JohnTheCoolingFan <ivan8215145640@gmail.com>
2022-09-10 21:56:50 +0300

    unit tick

--- src/main.rs
+++ src/main.rs
@@ -22,6 +22,13 @@ pub struct Movement {
 
 pub struct GameTickTimer(Timer);
 
+pub struct UnitHandle<'a> {
+    movement: &'a mut Movement
+}
+
+impl LuaUserData for UnitHandle<'_> {
+}
+
 fn spawn_unit(mut commands: Commands) {
     let lua = Lua::new();
     commands.spawn()
@@ -38,11 +45,31 @@ fn handle_movement(mut units: Query<(&mut Movement, &mut Transform), With<Unit>>
     }
 }
 
+fn unit_tick(mut units: Query<(&LuaState, &mut Movement), With<Unit>>, mut game_tick_timer: ResMut<GameTickTimer>, time: Res<Time>) {
+    if game_tick_timer.0.tick(time.delta()).just_finished() {
+        for (lua, mut movement) in units.iter_mut() {
+            let lua_lock = lua.0.lock().unwrap();
+            {
+                let globals = lua_lock.globals();
+                if let Some(on_tick) = globals.get::<_, Option<LuaFunction>>("on_tick").unwrap() {
+                    lua_lock.scope(|s| {
+                        let handle = UnitHandle{movement: &mut movement};
+                        let lua_handle = s.create_nonstatic_userdata(handle)?;
+                        on_tick.call(lua_handle)?;
+                        Ok(())
+                    }).unwrap();
+                };
+            };
+        }
+    }
+}
+
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
         .insert_resource(GameTickTimer(Timer::from_seconds(1.0/60.0, true)))
         .add_startup_system(spawn_unit)
         .add_system(handle_movement)
+        .add_system_to_stage(CoreStage::PreUpdate, unit_tick)
         .run();
 }
