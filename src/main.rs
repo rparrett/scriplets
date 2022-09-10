305e2cfd4a851b93e1b3da98c6f7c984c9287331
JohnTheCoolingFan <ivan8215145640@gmail.com>
2022-09-10 21:04:20 +0300

    New movement system

--- src/main.rs
+++ src/main.rs
@@ -17,28 +17,29 @@ pub struct Unit;
 #[derive(Component)]
 pub struct Movement {
     speed: f32,
-    cooldown_timer: Timer
-}
-
-impl Movement {
-    fn new(speed: f32) -> Self {
-        let cooldown_timer = Timer::from_seconds(60.0 / speed, false);
-        Self{speed, cooldown_timer}
-    }
+    next_move: Vec2
 }
 
 fn spawn_unit(mut commands: Commands) {
     let lua = Lua::new();
     commands.spawn()
         .insert(Unit)
+        .insert(Movement{speed:1.0, next_move: Vec2::splat(0.0)})
         .insert(LuaState::new(lua))
-        .insert(Movement::new(60.0))
         .insert_bundle(TransformBundle::default());
+}
+
+fn handle_movement(mut units: Query<(&mut Movement, &mut Transform), With<Unit>>) {
+    for (mut movement, mut transform) in units.iter_mut() {
+        transform.translation += movement.next_move.extend(0.0).clamp_length_max(1.0) * movement.speed;
+        movement.next_move = Vec2::splat(0.0);
+    }
 }
 
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
         .add_startup_system(spawn_unit)
+        .add_system(handle_movement)
         .run();
 }
