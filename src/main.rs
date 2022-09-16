90aa475ac1586db02710c1f120b355b9e6540211
JohnTheCoolingFan <ivan8215145640@gmail.com>
2022-09-16 18:18:00 +0300

    Spawn walls, add rapier plugins

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use std::sync::Mutex;
 use mlua::prelude::*;
 use bevy::{prelude::*, window::PresentMode, render::camera::ScalingMode, input::mouse::{MouseWheel, MouseScrollUnit, MouseMotion}};
+use bevy_rapier2d::prelude::*;
 
 const CLEAR_COLOR: Color = Color::rgb(0.1, 0.1, 0.1);
 const RESOLUTION: f32 = 16.0 / 9.0;
@@ -26,6 +27,7 @@ pub struct Movement {
 pub struct GameTickTimer(Timer);
 
 pub struct UnitSprite(Handle<Image>);
+pub struct WallSprite(Handle<Image>);
 
 pub struct UnitHandle<'a> {
     movement: &'a mut Movement
@@ -95,6 +97,30 @@ fn spawn_unit(mut commands: Commands, unit_sprite: Res<UnitSprite>) {
             ..default()});
 }
 
+fn spawn_walls(mut commands: Commands, wall_sprite: Res<WallSprite>) {
+    for i in 0..=5 {
+        spawn_wall(&mut commands, i as f32, 5.0, &wall_sprite.0)
+    }
+    for j in 0..=4 {
+        spawn_wall(&mut commands, 5.0, j as f32, &wall_sprite.0)
+    }
+}
+
+fn spawn_wall(commands: &mut Commands, x: f32, y: f32, sprite: &Handle<Image>) {
+    let transform = TransformBundle::from(Transform::from_xyz(x, y, 0.0));
+    commands.spawn()
+        .insert_bundle(SpriteBundle {
+            texture: sprite.clone(),
+            transform: transform.local,
+            global_transform: transform.global,
+            sprite: Sprite {
+                custom_size: Some(Vec2::splat(1.0)),
+                ..default()
+            },
+            ..default()
+        });
+}
+
 fn print_unit_positions(units: Query<&Transform, With<Unit>>) {
     for (i, unit) in units.iter().enumerate() {
         println!("Unit #{}: {}, {}", i, unit.translation.x, unit.translation.y);
@@ -132,6 +158,8 @@ fn unit_tick(mut units: Query<(&LuaState, &mut Movement), With<Unit>>, mut game_
 fn load_sprites(mut commands: Commands, assets: Res<AssetServer>) {
     let unit_sprite = assets.load("unit.png");
     commands.insert_resource(UnitSprite(unit_sprite));
+    let wall_sprite = assets.load("wall.png");
+    commands.insert_resource(WallSprite(wall_sprite));
 }
 
 fn main() {
@@ -147,8 +175,11 @@ fn main() {
             ..default()
         })
         .add_plugins(DefaultPlugins)
+        .add_plugin(RapierPhysicsPlugin::<NoUserData>::pixels_per_meter(32.0))
+        .add_plugin(RapierDebugRenderPlugin::default())
         .insert_resource(GameTickTimer(Timer::from_seconds(1.0/60.0, true)))
         .add_startup_system_to_stage(StartupStage::PreStartup, load_sprites)
+        .add_startup_system(spawn_walls)
         .add_startup_system(spawn_unit)
         .add_startup_system(spawn_camera)
         .add_system_to_stage(CoreStage::PreUpdate, unit_tick)
