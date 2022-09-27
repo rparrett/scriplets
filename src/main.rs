7be320ff7cd429cf3bb217d44441cadbf059ff40
JohnTheCoolingFan <ivan8215145640@gmail.com>
2022-09-27 22:59:27 +0300

    move lua api impls into LuaUnitHandle

--- src/main.rs
+++ src/main.rs
@@ -121,24 +121,28 @@ pub struct UnitHandle<'a> {
     game_clock: &'a GameClock
 }
 
+pub struct LuaUnitHandle<'a> {
+    handle: UnitHandle<'a>
+}
+
 // TODO: after making a planet map, methods for getting nearest transition tile or a tile adjacent
 //  to transition tile
-impl LuaUserData for UnitHandle<'_> {
+impl LuaUserData for LuaUnitHandle<'_> {
     fn add_methods<'lua, M: LuaUserDataMethods<'lua, Self>>(methods: &mut M) {
-        methods.add_method_mut("move", |_lua, handle, args: (f32, f32)| {
-            if let Some(movement) = &mut handle.movement {
+        methods.add_method_mut("move", |_lua, lua_handle, args: (f32, f32)| {
+            if let Some(movement) = &mut lua_handle.handle.movement {
                 movement.input_move = Vec2::from(args);
             };
             Ok(())
         });
-        methods.add_method_mut("rotate", |_lua, handle, rot: f32| {
-            if let Some(movement) = &mut handle.movement {
+        methods.add_method_mut("rotate", |_lua, lua_handle, rot: f32| {
+            if let Some(movement) = &mut lua_handle.handle.movement {
                 movement.input_rotation = rot;
             }
             Ok(())
         });
-        methods.add_method_mut("toggle_hand_brake", |_lua, handle, ()| {
-            if let Some(movement) = &mut handle.movement {
+        methods.add_method_mut("toggle_hand_brake", |_lua, lua_handle, ()| {
+            if let Some(movement) = &mut lua_handle.handle.movement {
                 movement.hand_brake = !movement.hand_brake;
             }
             Ok(())
@@ -146,23 +150,23 @@ impl LuaUserData for UnitHandle<'_> {
     }
 
     fn add_fields<'lua, F: LuaUserDataFields<'lua, Self>>(fields: &mut F) {
-        fields.add_field_method_get("time_since_start", |_lua, handle| {
-            Ok(handle.clock.0.elapsed_secs())
+        fields.add_field_method_get("time_since_start", |_lua, lua_handle| {
+            Ok(lua_handle.handle.clock.0.elapsed_secs())
         });
-        fields.add_field_method_get("global_time", |_lua, handle| {
-            Ok(handle.game_clock.0.elapsed_secs())
+        fields.add_field_method_get("global_time", |_lua, lua_handle| {
+            Ok(lua_handle.handle.game_clock.0.elapsed_secs())
         });
-        fields.add_field_method_get("gps", |lua, handle| {
-            let position: [f32; 2] = handle.transform.translation.truncate().into();
-            let rotation_radians = handle.transform.rotation.to_euler(EulerRot::XYZ).2;
+        fields.add_field_method_get("gps", |lua, lua_handle| {
+            let position: [f32; 2] = lua_handle.handle.transform.translation.truncate().into();
+            let rotation_radians = lua_handle.handle.transform.rotation.to_euler(EulerRot::XYZ).2;
             let rotation_degrees = -(rotation_radians * 180.0) / PI;
             let table = lua.create_table()?;
             table.set("position", position)?;
             table.set("rotation", rotation_degrees)?;
             Ok(table)
         });
-        fields.add_field_method_get("movement", |lua, handle| {
-            if let Some(movement) = &handle.movement {
+        fields.add_field_method_get("movement", |lua, lua_handle| {
+            if let Some(movement) = &lua_handle.handle.movement {
                 let movement_type = movement.movement_type.as_ref();
                 let speed = movement.speed;
                 let max_speed = movement.max_speed;
@@ -395,7 +399,7 @@ fn unit_tick(
                         clock: &clock,
                         game_clock: &game_clock
                     };
-                    let lua_handle = s.create_nonstatic_userdata(handle)?;
+                    let lua_handle = s.create_nonstatic_userdata(LuaUnitHandle{handle})?;
                     on_tick.call(lua_handle)?;
                     Ok(())
                 }).unwrap();
